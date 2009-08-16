b6860882436b26f0fa1670863f53d57587ba4d0b
Evan McClain <aeroevan@gmail.com>
2009-08-15 23:00:41 -0400

    Add -c option to notify current song only.

--- src/xmms2-notify.c
+++ src/xmms2-notify.c
@@ -1,3 +1,23 @@
+/*
+ * xmms2-notify.c - xmms2 libnotify client
+ *
+ * Copyright © 2009 Evan McClain <evan.mcclain@gatech.edu>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 #include <stdlib.h>
 #include <xmmsclient/xmmsclient.h>
 #include <xmmsclient/xmmsclient-glib.h>
@@ -6,9 +26,11 @@
 
 #define UNKNOWN "Unknown"
 #define NOTE "\xe2\x99\xab"
+#define USAGE "Notify currently playing song.\n -c: will only notify once and exit,\n default is to run until xmms2 is quit.\n"
 
 typedef struct track_t {
     gint status;
+    gint id;
     gint tracknr;
     gchar *album;
     gchar *artist;
@@ -20,6 +42,8 @@ typedef struct track_t {
 xmmsc_connection_t *conn = NULL;
 track current;
 
+/** Notify current song.
+ */
 void
 notify_song()
 {
@@ -35,7 +59,9 @@ notify_song()
         n = NULL;
     }
 
-    if(current.tracknr > 0)
+    /** We only notify if we have a valid id.
+     */
+    if(current.id != 0)
     {
 
         tmp = g_markup_printf_escaped("<big><b>%s</b></big>\n"
@@ -74,9 +100,14 @@ notify_song()
 
         notify_notification_set_timeout(n, 5000);
         notify_notification_show(n, NULL);
+        notify_notification_close(n, NULL);
+        g_object_unref(n);
+        n = NULL;
     }
 }
 
+/** Free stuff stored in the current track
+ */
 void
 current_track_unref()
 {
@@ -122,6 +153,8 @@ on_bindata_retrieve(xmmsv_t *res, void *udata)
         current.cover = gdk_pixbuf_loader_get_pixbuf(loader);
     }
     notify_song();
+
+    return 1;
 }
 
 void
@@ -191,11 +224,11 @@ int
 on_current_id(xmmsv_t *val, void *udata)
 {
     xmmsc_result_t *res;
-    int32_t id = -1;
 
-    xmmsv_get_int(val, &id);
+    if(!xmmsv_get_int(val, &current.id))
+        current.id = 0;
 
-    res = xmmsc_medialib_get_info(conn, id);
+    res = xmmsc_medialib_get_info(conn, current.id);
     xmmsc_result_notifier_set(res, on_medialib_info, udata);
     xmmsc_result_unref(res);
 
@@ -219,10 +252,29 @@ on_disconnect(void *arg)
 }
 
 int
-main()
+main(int argc, char **argv)
 {
     GMainLoop *ml;
     xmmsc_result_t *res;
+    xmmsv_t *returnval, *dict;
+    int c;
+    int flag_current = 0;
+    const char *err_buf;
+
+    while((c = getopt(argc, argv, "ch")) != -1)
+    {
+        switch(c)
+        {
+            case 'c':
+                flag_current = 1;
+                break;
+            case 'h':
+                printf(USAGE);
+                return EXIT_SUCCESS;
+            default:
+                return EXIT_FAILURE;
+        }
+    }
 
     conn = xmmsc_init("XMMS2-Notify");
     notify_init("XMMS2-Notify");
@@ -234,28 +286,63 @@ main()
         return EXIT_FAILURE;
     }
 
-    ml = g_main_loop_new(NULL, FALSE);
+    if(!flag_current)
+    {
 
-    res = xmmsc_playback_current_id(conn);
-    xmmsc_result_notifier_set(res, on_current_id, ml);
-    xmmsc_result_unref(res);
+        ml = g_main_loop_new(NULL, FALSE);
 
-    res = xmmsc_broadcast_playback_current_id(conn);
-    xmmsc_result_notifier_set(res, on_current_id, ml);
-    xmmsc_result_unref(res);
+        res = xmmsc_playback_current_id(conn);
+        xmmsc_result_notifier_set(res, on_current_id, ml);
+        xmmsc_result_unref(res);
 
-    res = xmmsc_playback_status(conn);
-    xmmsc_result_notifier_set(res, on_status_change, ml);
-    xmmsc_result_unref(res);
+        res = xmmsc_broadcast_playback_current_id(conn);
+        xmmsc_result_notifier_set(res, on_current_id, ml);
+        xmmsc_result_unref(res);
 
-    res = xmmsc_broadcast_playback_status(conn);
-    xmmsc_result_notifier_set(res, on_status_change, ml);
-    xmmsc_result_unref(res);
+        res = xmmsc_playback_status(conn);
+        xmmsc_result_notifier_set(res, on_status_change, ml);
+        xmmsc_result_unref(res);
 
-    xmmsc_disconnect_callback_set(conn, on_disconnect, ml);
+        res = xmmsc_broadcast_playback_status(conn);
+        xmmsc_result_notifier_set(res, on_status_change, ml);
+        xmmsc_result_unref(res);
 
-    xmmsc_mainloop_gmain_init(conn);
-    g_main_loop_run(ml);
+        xmmsc_disconnect_callback_set(conn, on_disconnect, ml);
+
+        xmmsc_mainloop_gmain_init(conn);
+        g_main_loop_run(ml);
+    }
+    else
+    {
+        res = xmmsc_playback_status(conn);
+        xmmsc_result_wait(res);
+        returnval = xmmsc_result_get_value(res);
+        if(xmmsv_is_error(returnval) &&
+                xmmsv_get_error(returnval, &err_buf))
+        {
+            fprintf(stderr, "playback current id returns error, %s\n",
+                    err_buf);
+	}
+        on_status_change(returnval, NULL);
+
+
+        res = xmmsc_playback_current_id(conn);
+        xmmsc_result_wait(res);
+        returnval = xmmsc_result_get_value(res);
+        if(xmmsv_is_error(returnval) &&
+                xmmsv_get_error(returnval, &err_buf))
+        {
+            fprintf(stderr, "playback current id returns error, %s\n",
+                    err_buf);
+	}
+        xmmsv_get_int(returnval, &current.id);
+
+        res = xmmsc_medialib_get_info(conn, current.id);
+        xmmsc_result_wait(res);
+        returnval = xmmsc_result_get_value(res);
+        dict = xmmsv_propdict_to_dict(returnval, NULL);
+        set_current_track(dict, NULL);
+    }
 
     notify_uninit();
     xmmsc_unref(conn);
